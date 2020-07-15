75944557e6ebafe1a541e25bfc743bad798604f5
geiser <geiser.gcc@gmail.com>
2020-07-15 16:49:26 -0300

    upload giant files

---
+++ tutorial/download-code/player/rlplayer.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5451d4ea13203dc5408b95917ddc3b8578b7f2c6a494c73ae243ac3462f5aeb3
+size 514065
---
+++ tutorial/executing-shiny-app/rlprez.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c934709ee96d8a72bd24b1765bb1fce5a9a2b79487b9b2cfae2352539aadb28
+size 65577
