e490cf78357ca12f470a9c62f47f6f2574374809
geiser <geiser.gcc@gmail.com>
2020-09-27 06:33:09 -0300

    chaning version

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 if (!'remotes' %in% rownames(installed.packages())) install.packages('remotes')
 if (!"rshinystatistics" %in% rownames(installed.packages())) {
   remotes::install_github("geiser/rshinystatistics")
-} else if (packageVersion("rshinystatistics") < "0.0.0.9029") {
+} else if (packageVersion("rshinystatistics") < "0.0.0.9030") {
   remotes::install_github("geiser/rshinystatistics")
 }
 
